7e0ec8a5e6c796d8703412a6f0efd880876bed6a
Mark Teekman <mark.teekman@gmail.com>
2022-11-23 20:34:03 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,24 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { createHead } from '@vueuse/head'
 import App from './App.vue'
+import router from '~/router'
 
 // register accessible components globally
 import components from 'accessible-vue-components'
 
-// project specific and component specific styles
+// project and component specific styles
 import '~/assets/scss/globals.scss'
 import './index.css'
+import 'nprogress/nprogress.css'
 import 'accessible-vue-components/dist/style.css'
 
 const app = createApp(App)
+app.use(createPinia())
+app.use(createHead())
+app.use(router)
 app.use(components)
-app.mount('#app')
+
+router.isReady().then(() => {
+  app.mount('#app')
+})
