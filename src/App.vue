1f5ba7e7c097c53058a3d8f05fe3c4309edc1666
Mark Teekman <mark.teekman@gmail.com>
2022-11-23 20:33:49 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,3 @@
-<script setup>
-import { ref } from 'vue';
-import IconAccessibility from '~icons/carbon/accessibility-alt'
-
-const count = ref(0)
-</script>
-
 <template>
-  <header>
-    <SkipLinks />
-  </header>
-  <main id="main-content" class="container h-screen">
-    <div class="align-center gap-8">
-      <MagicText>
-        <template #magic>
-          Accessible
-        </template>
-          Vue Starter
-      </MagicText>
-      <i-carbon-accessibility-alt style="display: block; font-size: 10rem;" />
-      <button class="text-white button" type="button" @click="count++">count is {{ count }}</button>
-    </div>
-  </main>
+  <router-view />
 </template>
-
-<style>
-  main {
-    display: grid;
-    place-items: center;
-    grid-gap: 3rem;
-  }
-</style>
