037e84d0ed20900fb3f5be5339ec8e63aa342dfd
Mark Teekman <mark.teekman@gmail.com>
2022-11-23 20:34:09 +0100

    Create example-page.vue

---
+++ src/pages/example-page.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="py-8">
+    <div class="container space-content">
+      <h1>Example Page</h1>
+      <p>This is an example page.</p>
+    </div>
+  </section>
+</template>
