10a98089469ff851217f54a61d21928a62974623
Mark Teekman <mark.teekman@gmail.com>
2022-11-23 20:34:12 +0100

    Create index.vue

---
+++ src/pages/index.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { ref } from 'vue';
+import IconAccessibility from '~icons/carbon/accessibility-alt'
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="align-center gap-8">
+    <MagicText>
+      <template #magic>
+        Accessible
+      </template>
+        Vue Starter
+    </MagicText>
+    <i-carbon-accessibility-alt style="display: block; font-size: 10rem;" />
+    <button class="text-white button" type="button" @click="count++">count is {{ count }}</button>
+  </div>
+</template>
+
+<route lang="yaml">
+meta:
+  layout: home
+</route>
