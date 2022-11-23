da5371f585af091de9f37bad5c1d088995717225
Mark Teekman <mark.teekman@gmail.com>
2022-11-23 20:34:00 +0100

    Create home.vue

---
+++ src/layouts/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <TheHeader />
+  <main id="main-content">
+    <router-view />
+  </main>
+  <TheFooter />
+</template>
+
+<style scoped>
+  main {
+    display: grid;
+    place-items: center;
+    grid-gap: 3rem;
+  }
+</style>
