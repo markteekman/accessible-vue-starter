e1e06a3534bb37468d534dd353fde0c66fd55ff2
Mark Teekman <mark.teekman@gmail.com>
2022-11-29 16:48:53 +0100

    Update project.js

--- src/stores/project.js
+++ src/stores/project.js
@@ -2,6 +2,17 @@ import { defineStore } from 'pinia'
 
 export const useProjectStore = defineStore('project', {
   state: () => ({
-    projectName: 'Accessible Vue Starter'
-  })
+    projectName: 'Accessible Vue Starter',
+    projectDescription: 'A starter template for accessible Vue 3 apps',
+    projectTagline: {
+      main: 'Accessible',
+      sub: 'Vue Starter',
+    }
+  }),
+  actions: {
+    updateProjectTagline(newTagline) {
+      this.projectTagline.main = newTagline
+      this.projectTagline.sub = 'is the best!'
+    }
+  }
 })
