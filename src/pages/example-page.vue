5a29767df6673ce591b71a602cfd4dd3bf1c1222
Mark Teekman <mark.teekman@gmail.com>
2022-11-29 16:48:46 +0100

    Update example-page.vue

--- src/pages/example-page.vue
+++ src/pages/example-page.vue
@@ -1,5 +1,20 @@
+<script setup>
+import { useHead } from '@vueuse/head'
+import { useProjectStore } from '~/stores/project'
+
+useHead({
+  title: `Example page | ${useProjectStore().projectName}`,
+  meta: [
+    {
+      name: 'description',
+      content: 'This is an example page.'
+    }
+  ]
+})
+</script>
+
 <template>
-  <section class="py-8">
+  <section class="pt-16 pb-8">
     <div class="container space-content">
       <h1>Example Page</h1>
       <p>This is an example page.</p>
