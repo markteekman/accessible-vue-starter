fd914e0e1e7e71a6909a62ebc0de2002129213d0
Mark Teekman <mark.teekman@gmail.com>
2022-11-30 13:50:17 +0100

    Update TheFooter.vue

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -3,8 +3,8 @@ const currentYear = new Date().getFullYear()
 </script>
 
 <template>
-  <footer>
-    <div class="container py-4">
+  <footer class=" bg-zinc-900">
+    <div class="container py-8">
       <p>&copy; {{ currentYear }} - Starter Theme for <a href="https://vuejs.org/">Vue 3</a> and <a href="https://vitejs.dev/">Vite</a>.</p>
     </div>
   </footer>
